2be5e8bdf08249202032a0d03af2e294fe0800ee
Haybuka <paschal.nduleo@gmail.com>
2025-01-09 11:22:09 +0100

    word list storage

--- context/WordListProvider.tsx
+++ context/WordListProvider.tsx
@@ -30,25 +30,28 @@ const WordListProvider = ({ children }: WordListProp) => {
   const [wordList, setWordList] = useState([] as wordListsType);
 
   const handleAddWordToList = async (word: wordType) => {
-    const wordExists = wordList.filter((w) => w?.word === word?.word);
-    if (wordExists.length >= 1) {
-      Toast.show({
-        type: 'success',
-        text1: `Word Exists`,
-      });
-    } else {
-      const toUpperCase =
-        word?.word.slice(0, 1).toUpperCase() + word.word.slice(1);
+    try {
+      const wordExists = wordList?.filter((w) => w?.word === word?.word);
+      if (wordExists?.length >= 1) {
+        Toast.show({
+          type: 'success',
+          text1: `Word Exists`,
+        });
+      } else {
+        const toUpperCase =
+          word?.word?.slice(0, 1).toUpperCase() + word?.word?.slice(1);
 
-      const newList = [word, ...wordList];
-      await setStorageItem('word-list', newList);
+        const newList = [word, ...wordList];
 
-      setWordList(newList);
-      Toast.show({
-        type: 'success',
-        text1: `${toUpperCase} added.`,
-      });
-    }
+        await setStorageItem('@Word_list', JSON.stringify(newList));
+
+        setWordList(newList);
+        Toast.show({
+          type: 'success',
+          text1: `${toUpperCase} added.`,
+        });
+      }
+    } catch (error) {}
   };
 
   const handleRemoveWordFromList = (removeWord: wordType) => {
@@ -63,13 +66,28 @@ const WordListProvider = ({ children }: WordListProp) => {
   };
 
   const getStorageData = async (key: string) => {
-    const wordStorage = await getStorageData(key);
-    console.log(wordStorage, 'word storage');
+    try {
+      const wordStorage = (await AsyncStorage.getItem(key)) as string;
+
+      if (wordStorage !== null) {
+        return JSON.parse(wordStorage);
+      } else {
+        return { title: undefined, items: undefined };
+      }
+    } catch (error) {}
   };
 
   useEffect(() => {
-    getStorageData('word-list');
-  }, []);
+    const list = getStorageData('@Word_list');
+    console.log(list, 'word');
+  }, [wordList]);
+  // useEffect(() => {
+  //   const wordStorage = getStorageData(
+  //     '@Word_list'
+  //   ) as unknown as wordListsType;
+  //   setWordList(wordStorage);
+  //   console.log(wordStorage, 'word storage');
+  // }, []);
   return (
     <WordListContext.Provider value={values}>
       {children}
